8e0608f5a9af3589dda3d20e211fd24f75bc1338
luyuan <1324725249@qq.com>
2022-09-28 13:33:23 +0800

    remove irita,opb

--- client.go
+++ client.go
@@ -4,18 +4,15 @@ import (
 	. "github.com/kaifei-bianjie/common-parser"
 	"github.com/kaifei-bianjie/iritachain-mod-parser/codec"
 	"github.com/kaifei-bianjie/iritachain-mod-parser/modules/evm"
-	"github.com/kaifei-bianjie/iritachain-mod-parser/modules/opb"
 )
 
 type MsgClient struct {
 	Evm Client
-	Opb Client
 }
 
 func NewMsgClient() MsgClient {
 	codec.MakeEncodingConfig()
 	return MsgClient{
 		Evm: evm.NewClient(),
-		Opb: opb.NewClient(),
 	}
 }
--- modules/types.go
+++ modules/types.go
@@ -1,22 +1,14 @@
 package msgs
 
 import (
-	opb "github.com/bianjieai/irita/modules/opb/types"
 	evm "github.com/tharsis/ethermint/x/evm/types"
 )
 
 const (
 	MsgTypeEthereumTx = "ethereum_tx"
-
-	MsgTypeMint    = "mint"
-	MsgTypeReclaim = "reclaim"
 )
 
 type (
 	//evm
 	MsgEthereumTx = evm.MsgEthereumTx
-
-	//opb
-	MsgMint    = opb.MsgMint
-	MsgReclaim = opb.MsgReclaim
 )
